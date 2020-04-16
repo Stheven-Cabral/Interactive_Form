703f96f64b525b8ae413df7e5366643759ffd761
Stheven Cabral <stheven.cabral@gmail.com>
2020-04-16 16:22:40 -0700

    04162020  1622

---
+++ js/script.js
@@ -0,0 +1,57 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 3 - Interactive Form
+******************************************/
+
+
+/***
+ * 'nameInput' global variable captures the '#name' input element.
+ * 'otherJobRole' global variable captures the '#title' select element.
+ ***/
+const nameInput = document.getElementById('name');
+const otherJobRole = document.getElementById('other-title');
+
+
+/***
+ * 'createFocus' function - adds focus to an element.
+ * @param {input} nameInput - accepts the input element to focus on.
+ * Called 'createFocus' function on 'nameInput' global variable.
+ ***/
+
+function createFocus(inputElement) {
+    inputElement.focus();
+}
+
+createFocus(nameInput);
+
+
+/***
+ * 'hideOtherRoleInput' function - hides the '#other=title' text input.
+ * Called the 'hideOtherRoleInput' function.
+ ***/
+
+function hideOtherRoleInput() {
+    otherJobRole.style.display = 'none';
+}
+
+hideOtherRoleInput();
+
+
+/***
+ * 'displayOtherRoleInput' function - displays the '#other=title' text input if a user selects the value 'other'.
+ * Called the 'displayOtherRoleInput' function.
+ ***/
+
+function displayOtherRoleInput() {
+    const jobSelect = document.getElementById('title');
+    const jobSelectOption = document.querySelectorAll('#title option');
+    jobSelect.addEventListener('change', () => {
+        if (jobSelect.value === 'other') {
+            otherJobRole.style.display = 'inherit';
+        } else {
+            otherJobRole.style.display = 'none';
+        };
+    });
+}
+
+displayOtherRoleInput();
