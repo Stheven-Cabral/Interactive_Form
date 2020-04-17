ce87356864feaa6ac177f4a3be332a8600953bfc
Stheven Cabral <stheven.cabral@gmail.com>
2020-04-17 15:44:33 -0700

    04172020  1544

--- js/script.js
+++ js/script.js
@@ -7,11 +7,13 @@ FSJS project 3 - Interactive Form
 /***
  * 'nameInput' global variable captures the '#name' input element.
  * 'otherJobRole' global variable captures the '#title' select element.
- * 'colors' global variable captures the colors select element.
+ * 'colors' global variable captures the '#colors-js-puns' div for t-shirt colors.
+ * 'selectShirtTheme' global variable captures the '#design' select element.
  ***/
 const nameInput = document.getElementById('name');
 const otherJobRole = document.getElementById('other-title');
 const colors = document.getElementById('colors-js-puns');
+const selectShirtTheme = document.getElementById('design');
 
 
 /***
@@ -51,18 +53,65 @@ function displayOtherRoleInput() {
             otherJobRole.style.display = 'inherit';
         } else {
             otherJobRole.style.display = 'none';
-        };
+        }
     });
 }
 
 displayOtherRoleInput();
 
 
+/***
+ * 'hideShirtColors' function - hides the T-shirt color label and select menu.
+ * Called the 'hideShirtColors' function.
+ ***/
+
 function hideShirtColors() {
-    const selectShirtTheme = document.getElementById('design');
     if (selectShirtTheme.value === 'Select Theme') {
         colors.style.display = 'none';
     }
 }
 
-hideShirtColors();  
+hideShirtColors();
+
+
+/***
+ * 'shirtColors' function - displays the T-shirt color label and select menu when the design option chosen 
+    is anything other than 'Select Theme'. After a user selects a theme, only color options that match the design selected are displayed.
+ * Called the 'shirtColors' function.
+ ***/
+
+function shirtColors() {
+    const selectColor = document.getElementById('color');
+    const colorOptions = document.querySelectorAll('#color option');
+
+    selectShirtTheme.addEventListener('change', () => {
+        if (selectShirtTheme.value === 'js puns') {
+            selectColor.value = colorOptions[0].value;
+            for (let p = 0; p < colorOptions.length; p += 1) {
+                if (p > 2) {
+                    colorOptions[p].style.display = 'none';
+                } else {
+                    colorOptions[p].style.display = 'inherit';
+                }
+            }
+        } else if (selectShirtTheme.value === 'heart js') {
+            selectColor.value = colorOptions[3].value;
+            for (let h = 0; h < colorOptions.length; h += 1) {
+                if (h <= 2) {
+                    colorOptions[h].style.display = 'none';
+                } else {
+                    colorOptions[h].style.display = 'inherit';
+                }
+            }
+        }
+        if (selectShirtTheme.value === 'js puns' || selectShirtTheme.value === 'heart js') {
+            colors.style.display = 'inherit';
+        } else {
+            hideShirtColors();
+        }
+    });
+}
+
+shirtColors();
+
+
